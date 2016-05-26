7f0042af9b430d49e36c09da0156a8ad6b9a75a9
David Amaro <dav1494@ciencias.unam.mx>
2016-05-26 14:28:13 -0500

    Se añade código que genera la representación matricial que más me gusta. Aunque no sea irreducible.

---
+++ representaciones.jl
@@ -0,0 +1,41 @@
+__precompile__(true)
+
+module RE
+
+export matriz, generarPerm, generarBase
+export aplicacion, representacion
+
+function matriz(base, aplicado)
+    res = zeros(length(base), length(aplicado))
+    for i in 1:length(base)
+        for j in 1:length(aplicado)
+            res[i,j] = dot(base[i],aplicado[j])
+        end
+    end
+    res
+end
+
+function generarPerm(estado)
+    collect(permutations(estado))
+end
+
+function generarBase(qbits)
+    estados = Array{Int64,1}[]
+    base = zeros(qbits)
+    for i in 1:qbits
+        base[i] = 1
+        push!(estados,base)
+        base = zeros(qbits)
+    end
+    estados
+end
+
+function aplicacion(elemento, listaPerm)
+    [x[elemento] for x in listaPerm]
+end
+
+function representacion(elemento,dim = 3)
+    matriz(generarBase(dim),aplicacion(elemento,map(generarPerm,generarBase(dim))))
+end
+
+end
